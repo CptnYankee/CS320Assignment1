bbe2517c3d1670b8b373557f90938c5731a56acc
JStanderfer <jjstanderfer@gmail.com>
2019-01-25 19:13:35 -0800

    Create BusScheduleApp.java

---
+++ BusScheduleApp.java
@@ -0,0 +1,45 @@
+package assignmentOne;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.util.HashMap;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.net.*;
+
+public class BusScheduleApp {
+
+	public static void main(String[] args) throws Exception {
+		
+		HashMap<String, BusRoute> routes = new HashMap<>();
+		
+		Scanner sysin = new Scanner(System.in);
+		String entry;
+		
+		RouteFinder rf = new RouteFinder();
+		rf.initialConnection();
+		
+		System.out.println("Please Enter the first letter of your destination.");
+		entry = sysin.next(Pattern.compile("[a-zA-Z]"));
+		
+		if(rf.destinationSearch(entry,routes)) {
+			System.out.print(routes.values());
+		}else {
+			System.out.println("There are no destinations that begin with that letter");
+		}
+		
+		System.out.println("Which route would you like to view?");
+		entry = sysin.next();
+		
+		if(rf.subConnection(entry)) {
+			System.out.println("Success! Probably");
+		}else {
+			System.out.println("Invalid");
+		}
+		
+		sysin.close();
+
+	}
+
+}

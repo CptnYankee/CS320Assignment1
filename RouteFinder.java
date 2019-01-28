5c02a4d8bdaba83aac13a55ef262f03c27169361
JStanderfer <jjstanderfer@gmail.com>
2019-01-27 22:27:50 -0800

    Update RouteFinder.java

--- RouteFinder.java
+++ RouteFinder.java
@@ -7,7 +7,12 @@ import java.util.HashMap;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.net.*;
-
+/************************************************************************************************
+ * @author Joshua Standerfer																	*
+ * This method creates a connection to https://www.communitytransit.org/busservice/schedules/	*
+ * and stores the page's source code as a string. It then performs regex operations in order to *
+ * retrieve useful information about bus routes and their stops.								*
+ **********************************************************************************************	*/
 public class RouteFinder{
     String schedules;
     String routeSchedule;
@@ -56,6 +61,8 @@ public class RouteFinder{
 		boolean valid = false;
 		try {
 			//System.out.println(url + "/route/" + subRoute );
+			//all /'s in the route names are replaced with -
+			subRoute = subRoute.replaceAll("/","-");
 			bc = new URL(url + "/route/" + subRoute).openConnection();
 			valid = true;
 		} catch (MalformedURLException e) {
@@ -87,6 +94,8 @@ public class RouteFinder{
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+		//I have no idea why this prints the URL out twice...
+		System.out.println(url + "/route/" + subRoute);
 		return valid;
 	}
 	//Patterns:
@@ -99,6 +108,8 @@ public class RouteFinder{
 	//The last entry in each destination is different.
 	//previously had a group (subMatcher group 1) for each URL, but the format allows us to append the route# to the end
 	
+	//This is the method which finds the list of potential destinations based on user input.
+	//It will print a list of locations and the lines that go to them
 	boolean destinationSearch(String s, HashMap<String, BusRoute> routes) {
 		
 		String subStr;
@@ -139,19 +150,65 @@ public class RouteFinder{
 	//<strong class="fa fa-stack-1x">C</strong>
 	//<p>Boeing Gate E68</p>
 
-
-	boolean routeDetails(String subRoute) {
+	//This is the method which returns details to the user about any route they enter.
+	//Will not accept invalid routes.
+	boolean routeDetails(String subRoute)throws Exception {
 		if(!subConnection(subRoute)){
 			return false;
 		}
-		String[] stopNumA;
-		String[] stopAddrA;
+		//TO A
+		int alength = 0;
+		String toA;
+		String[] stopNumA = new String[20];
+		String[] stopAddrA = new String[20];
+		//TO B -- these are usually just the stops in reverse order.
+		int blength = 0;
+		String toB;
+		String[] stopNumB = new String[20];
+		String[] stopAddrB = new String[20];
 		
-		String[] stopNumB;
-		String[] stopAddrB;
+		String routeP = "<h2>Weekday<small>(.*?)</small></h2>.*?<tr>(.*?)</tr>";
+		String stopNumP = "<strong class=\"fa fa-stack-1x\">(.*?)</strong>.*?<p>(.*?)</p>.*?</th>";
+		//String stopAddrP = "<p>(.*?)</p>";
 		
-		String routePattern = "<h2>Weekday<small>(.*?)</small></h2>.*?<tr>(.*?)</tr>";
+		Pattern pattern = Pattern.compile(routeP, Pattern.DOTALL);
+		Pattern numPattern = Pattern.compile(stopNumP, Pattern.DOTALL| Pattern.MULTILINE);
+		//Pattern addrPattern = Pattern.compile(stopAddrP, Pattern.DOTALL | Pattern.MULTILINE);
+
+		Matcher matcher = pattern.matcher(routeSchedule);
+		//Weekday To A
+		matcher.find();
+		toA = matcher.group(1);
+		int index = 0;
+		Matcher stopMatcher = numPattern.matcher(matcher.group(2));
 		
+		while(stopMatcher.find()) {
+			stopNumA[index] = stopMatcher.group(1);
+			stopAddrA[index] = stopMatcher.group(2);
+			index++;
+		}
+		alength = index;
+		//Weekday to B
+		matcher.find();
+		toB = matcher.group(1);
+		index = 0;
+		stopMatcher = numPattern.matcher(matcher.group(2));
+		
+		while(stopMatcher.find()) {
+			stopNumB[index] = stopMatcher.group(1);
+			stopAddrB[index] = stopMatcher.group(2);
+			index++;
+		}
+		blength = index;
+		System.out.println(toA);
+		System.out.println("++++++++++++++++++++++++++++");
+		for(int i = 0; i < alength; i++) {
+			System.out.println(stopNumA[i] + ": " + stopAddrA[i]);
+		}
+		System.out.println("+++++++++++++++++++++++++++++");
+		for(int i = 0; i < blength; i++) {
+			System.out.println(stopNumB[i] + ": " + stopAddrB[i]);
+		}
 		return true;
 	}
 }
